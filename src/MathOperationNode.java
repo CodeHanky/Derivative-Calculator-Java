9c56dde6a04a39b7ee06bce8bd1ea7970aaa0e18
CodeHanky <gitaliasub.g6non@simplelogin.fr>
2022-07-18 14:04:51 +0300

    Test with GCM

--- src/MathOperationNode.java
+++ src/MathOperationNode.java
@@ -5,7 +5,7 @@ public class MathOperationNode {
 	private String operator;
 	private ArrayList<String> operands = new ArrayList<>();
 	
-//Comment 3
+//Comment 4
 	public MathOperationNode(String operator, ArrayList<String> operands) {
 		super();
 		this.operator = operator;
